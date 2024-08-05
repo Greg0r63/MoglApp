a7b1a416b36788dae90226e186cadd50637d8eab
OrianeBatteux <oriane08@icloud.com>
2024-08-05 09:38:36 +0200

    Update ecran AvisEtSuggestion

--- MoglApp/MoglApp/Models/AvisSuggestionItem.swift
+++ MoglApp/MoglApp/Models/AvisSuggestionItem.swift
@@ -8,12 +8,12 @@
 import Foundation
 
 class AvisSuggestion {
-    var id : Int
+    var id : UUID = UUID()
     var note: Int
     var avis: String
     var suggestion: String
     
-    init(id: Int, note: Int, avis: String, suggestion: String) {
+    init(id: UUID = UUID(), note: Int, avis: String, suggestion: String) {
         self.id = id
         self.note = note
         self.avis = avis
