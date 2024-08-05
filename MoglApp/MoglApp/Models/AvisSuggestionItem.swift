16431c3af9ede9f6c4b1436d288efc08cac8b185
OrianeBatteux <oriane08@icloud.com>
2024-08-05 10:48:09 +0200

    DocC sur AvisSuggestion

--- MoglApp/MoglApp/Models/AvisSuggestionItem.swift
+++ MoglApp/MoglApp/Models/AvisSuggestionItem.swift
@@ -6,12 +6,29 @@
 //
 
 import Foundation
+/**
+ Documentation de la classe AvisSuggestion.
 
+ Cette classe représente des notes, avis et suggestion que l'utilisateur peut donner sur l'ensemble de l'application.
+
+ */
 class AvisSuggestion {
     var id : UUID = UUID()
+    // Note
     var note: Int
+    // Avis
     var avis: String
+    // Suggestion
     var suggestion: String
+    
+    /**
+     Initialise une instance de `AvisSuggestion` avec des valeurs spécifiques.
+
+     - Parameters:
+       - note: Note donner par l'utilisteur
+       - avis: Avis donner par l'utilisateur
+       - suggestion: Suggestion donner par l'utilisateur
+     */
     
     init(id: UUID = UUID(), note: Int, avis: String, suggestion: String) {
         self.id = id
