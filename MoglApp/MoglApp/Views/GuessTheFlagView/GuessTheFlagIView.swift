a6a99a903c1caf03e2a03d9cd26eee9eb32fb6cf
Dosso <massidedosso@gmail.com>
2024-09-15 16:29:34 +0200

    - Modification du CountriyViewModel qui récupère désormais les données d'une API créé a partir d'un fichier Json: - Ajout de la méthode fetchCountry - Modification de la vue GuessTheFlag pour que les données soient celles récupérer dans le view model

---
+++ MoglApp/MoglApp/Views/GuessTheFlagView/GuessTheFlagIView.swift
@@ -0,0 +1,74 @@
+//
+//  FlagFromAPIView.swift
+//  MoglApp
+//
+//  Created by Massidé Dosso on 13/09/2024.
+//
+
+import SwiftUI
+
+struct FlagFromAPIView: View {
+    @StateObject private var viewModel = CountryViewModel()
+    var body: some View {
+        ZStack {
+            LinearGradientView()
+            
+            
+        VStack {
+                VStack(spacing: 25) {
+                    VStack {
+                        
+                        Text("Appuie sur le drapeau du pays suivant:")
+                            .font(.title)
+                            .foregroundColor(Color.black)
+                            .multilineTextAlignment(.center)
+                            .foregroundStyle(.white)
+                        
+                        if !viewModel.country.isEmpty {
+                            Text(viewModel.country[viewModel.correctAnswer].name)
+                                .font(.largeTitle)
+                                .fontWeight(.heavy)
+                                .foregroundColor(Color("MOGLButton"))
+                                .padding(.top)
+                                .foregroundStyle(.white)
+                        } else {
+                            Text("Pas d'affichage")
+                        }
+                        
+                    }
+                }
+                
+            if viewModel.country.count >= 3 {
+                ForEach(0..<3) { number in
+                    Button(action: {
+                        viewModel.flagChoosed(number)
+                    }) {
+                        Text(viewModel.country[number].flag)
+                            .font(.system(size: 100))
+                            .padding(.bottom)
+                    }
+                    
+                }
+            } else {
+                Text("Chargement des drapeaux")
+            }
+
+        }
+        .onAppear{
+            viewModel.fetchCountries()
+        }
+        .alert(viewModel.answerChoosed, isPresented: $viewModel.showingScore) {
+            Button("Continuer", role: .none, action: viewModel.nextFlagChoosed)
+        } message: {
+            Text("Ton score est de \(viewModel.score)")
+        }
+            
+        }
+
+    }
+    }
+
+
+#Preview {
+    FlagFromAPIView()
+}
